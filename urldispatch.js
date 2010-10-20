e0f7c2d1cc471d8800d1a13c313ff50f7ba9f0ec
Török Gábor <gabor@20y.hu>
2010-10-20 17:09:03 +0200

    Add docstrings.

--- urldispatch.js
+++ urldispatch.js
@@ -7,38 +7,55 @@ dojo.declare('urldispatch.Dispatcher',
     {
         _routes: [],
 
-        constructor: function(routes) {
+        constructor: function(/*Array*/ routes) {
+            // summary:
+            //            Constructs Dispatcher object.
+            // description:
+            //            Builds inner routing table and subscribes itself
+            //            to location hash changes.
             this._parseRoutes(routes);
             dojo.subscribe("/dojo/hashchange", this, 'dispatch');
             this.dispatch(dojo.hash());
         },
 
-        _parseRoutes: function(routes) {
-            var that = this;
-            dojo.forEach(routes, function(route, idx) {
-                var args = route[0].match(/:\w+/g),
+        _parseRoutes: function(/*Array*/ routes) {
+            // summary:
+            //            Builds inner routing table.
+            // description:
+            //            Transforms abstract URL patterns to regular
+            //            expressions and collects keyword arguments.
+            dojo.forEach(routes, dojo.hitch(this, function(route) {
+                var kwArgs = route[0].match(/:\w+/g),
                     re = new RegExp('^' + route[0].replace(/(:\w+)/, '(\\w+)') + '$', 'g');
-                that._routes.push([re, route[1], args]);
-            });
+                this._routes.push([re, route[1], kwArgs]);
+            }));
         },
 
-        redirect: function(url) {
+        redirect: function(/*String*/ hash) {
+            // summary:
+            //            Redirects visitor to argument URL hash.
             dojo.hash(url);
         },
 
-        dispatch: function(hash) {
-            var i, j, route, params, context, arg, view;
+        dispatch: function(/*String*/ hash) {
+            // summary:
+            //            Calls view according to URL hash.
+            // description:
+            //            Finds routing rule that matches against the URL hash
+            //            specified and calls appropiate view function with
+            //            context arguments.  Exactly one view should belong to
+            //            each routes.
+            var i, j, route, params, context = {}, arg, view;
             for (i = 0; i < this._routes.length; i++) {
                 route = this._routes[i];
                 if (!route[0].test(hash)) {
                     continue;
                 }
                 params = hash.split(route[0]);
-                context = {};
-                params.shift();
+                params.shift(); // strip first and last empty chunk
                 params.pop();
                 for (j = 0; j < params.length; j++) {
-                    arg = route[2][j].substring(1);
+                    arg = route[2][j].substring(1); // strip first char ':'
                     context[arg] = params[j];
                 }
                 view = route[1];
