5ad0c826e48e31d1b47e4892affefc2b2beee2d9
Török Gábor <gabor@20y.hu>
2011-03-26 15:31:38 +0100

    Move route match unit test to tests.js module.

--- tests.js
+++ tests.js
@@ -21,5 +21,40 @@ doh.register('tests.urldispatchTestGroup', [
 	    doh.e(urldispatch.MissingArgumentError, this.dispatcher, 'reverse', ['hello']);
 	    doh.e(urldispatch.RouteNotFoundError, this.dispatcher, 'reverse', ['unknown_name']);
         }
+    },
+    {
+        name: 'routeMatchTest',
+        setUp: function(){
+        },
+        runTest: function(){
+            var test_views = {
+                    root : 0,
+                    hello : 0
+                },
+                routes = [
+                    ['!/', function() {
+                         test_views.root++;
+                    }, 'home'],
+                    ['!/hello', function() {
+                         test_views.hello++
+                    }, 'hello']
+                ],
+                dispatcher = new urldispatch.Dispatcher(routes);
+                doh.is(0, test_views.root);
+                dispatcher.dispatch("!/");
+                doh.is(1, test_views.root);
+                dispatcher.dispatch("!/hello");
+                doh.is(1, test_views.root);
+                doh.is(1, test_views.hello);
+                dispatcher.dispatch("!/");
+                doh.is(2, test_views.root);
+                doh.is(1, test_views.hello);
+                dispatcher.dispatch("!/hello");
+                doh.is(2, test_views.root);
+                doh.is(2, test_views.hello);
+                dispatcher.dispatch("!/");
+                doh.is(3, test_views.root);
+                doh.is(2, test_views.hello);
+        }
     }
-]);
+ ]);
