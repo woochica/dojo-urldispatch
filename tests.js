93fb209faab5b1ffab5c4e5330f96aae04170749
Török Gábor <gabor@20y.hu>
2011-03-26 15:45:30 +0100

    Move initialization code to `setUp()` for routeMatchTest.

--- tests.js
+++ tests.js
@@ -25,36 +25,33 @@ doh.register('tests.urldispatchTestGroup', [
     {
         name: 'routeMatchTest',
         setUp: function(){
+            this.test_views = {
+                root: 0,
+                hello: 0
+            };
+            this.routes = [
+                ['!/',  dojo.hitch(this, function(){ this.test_views.root++; }), 'home'],
+                ['!/hello',  dojo.hitch(this, function() { this.test_views.hello++; }), 'hello']
+            ];
+            this.dispatcher = new urldispatch.Dispatcher(this.routes);
         },
         runTest: function(){
-            var test_views = {
-                    root : 0,
-                    hello : 0
-                },
-                routes = [
-                    ['!/', function() {
-                         test_views.root++;
-                    }, 'home'],
-                    ['!/hello', function() {
-                         test_views.hello++
-                    }, 'hello']
-                ],
-                dispatcher = new urldispatch.Dispatcher(routes);
-                doh.is(0, test_views.root);
-                dispatcher.dispatch("!/");
-                doh.is(1, test_views.root);
-                dispatcher.dispatch("!/hello");
-                doh.is(1, test_views.root);
-                doh.is(1, test_views.hello);
-                dispatcher.dispatch("!/");
-                doh.is(2, test_views.root);
-                doh.is(1, test_views.hello);
-                dispatcher.dispatch("!/hello");
-                doh.is(2, test_views.root);
-                doh.is(2, test_views.hello);
-                dispatcher.dispatch("!/");
-                doh.is(3, test_views.root);
-                doh.is(2, test_views.hello);
+            var test_views = this.test_views;
+            doh.is(0, test_views.root);
+            this.dispatcher.dispatch("!/");
+            doh.is(1, test_views.root);
+            this.dispatcher.dispatch("!/hello");
+            doh.is(1, test_views.root);
+            doh.is(1, test_views.hello);
+            this.dispatcher.dispatch("!/");
+            doh.is(2, test_views.root);
+            doh.is(1, test_views.hello);
+            this.dispatcher.dispatch("!/hello");
+            doh.is(2, test_views.root);
+            doh.is(2, test_views.hello);
+            this.dispatcher.dispatch("!/");
+            doh.is(3, test_views.root);
+            doh.is(2, test_views.hello);
         }
     }
  ]);
